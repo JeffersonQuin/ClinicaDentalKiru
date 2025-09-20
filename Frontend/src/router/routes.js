dd44cdf7468aa8fe831f3bbd30be9b158aa836d3
JeffQuin <jeffersongervacioquinonezaguir@gmail.com>
2025-09-19 23:26:12 -0400

    Inicio del Proyecto

---
+++ Frontend/src/router/routes.js
@@ -0,0 +1,13 @@
+import MainLayout from 'layouts/MainLayout.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      // tus rutas hijas
+    ]
+  }
+]
+
+export default routes
